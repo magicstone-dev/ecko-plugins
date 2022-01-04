7d9fdf73abfb025eb9002d62b8a47247302964a9
Manish Sharma <arnoltherasing@gmail.com>
2022-01-04 14:35:45 +0600

    add specs for ecko plugins and coverage requirement (#5)

--- ecko-plugins/spec/spec_helper.rb
+++ ecko-plugins/spec/spec_helper.rb
@@ -5,13 +5,13 @@ SimpleCov.start do
   minimum_coverage 90
 end
 
-require "bundler/setup"
-require "ecko/plugins"
+require 'bundler/setup'
+require 'ecko/plugins'
+require 'specor/specor'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
-
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
